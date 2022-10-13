134858ede8658e44b1d8a5a43a78c0bdd748a09b
Lloyd Alex Porter <48352202+lloydalexporter@users.noreply.github.com>
2022-10-13 19:09:52 +0100

    Hopefully fixed all the weird tab issues.

--- CsvManagement.py
+++ CsvManagement.py
@@ -12,7 +12,7 @@ from pprint import pprint as pp
 
 
 class CsvManagement:
-	
+
 	# ! - Constants - ! #
 	TIME_FORMAT_STR = '%H:%M' # Format used for time data.
 	TWO_DP = Decimal(10) ** -2 # Set the number of decimal places.
@@ -39,20 +39,20 @@ class CsvManagement:
 		"Nov": 11,
 		"Dec": 12
 	} # Dictionary for replacing Month name with Month number.
-	
-	
+
+
 	# ! - Functions - ! #
 	def __init__(self): # Always run.
 		self.ifMain = ifMain = False # Initialise and set to False.
 		if __name__ == "__main__": self.ifMain = ifMain = True # If this is Main, set to True.
-	
-		
+
+
 	# >>> Create a 2D array.
 	def create2DArray(self, column, row):
 		array = [[0 for _ in range(column)] for _ in range(row)] # The code to create one, using the two parameters that were passed.
 		return array # Return this 2D array.
-	
-	
+
+
 	# >>> Clear arrays.
 	def clearArrays(self):
 		self.monthArray = []
@@ -61,11 +61,11 @@ class CsvManagement:
 		self.endArray = []
 		self.durationArray = []
 		self.paidHoursArray = []
-		
-	
+
+
 	# >>> Select csv file.
 	def selectFile(self):
-		
+
 		# >>> Get csv file path from user input and validate.
 		while True: # Loop until valid user input.
 			self.CSV_FILE = input("\nEnter the file path of the CSV file:\nFile path -> ").strip() # Ask for the csv file path.
@@ -74,11 +74,11 @@ class CsvManagement:
 			if exists(self.CSV_FILE): # If file exists, user input is valid,
 				return False # return False.
 			print("\n! >>> ERROR - File does not exist, try again.") # Say the user input is invalid.
-	
-	
+
+
 	# >>> Get the data from the csv file.
 	def getCSVData(self):
-		
+
 		# >>> Get the number of records.
 		self.recordCount = 0
 		with open(self.CSV_FILE, 'r') as csvFile: # Open the CSV file under csvFile.
@@ -91,8 +91,8 @@ class CsvManagement:
 							self.recordCount += 1 # Increment recordCount.
 		if self.ifMain: print("recordCount =", self.recordCount, end='\n\n')
 		csvFile.close() # Close the csvFile.
-		
-	
+
+
 		# >>> Get data from CSV file.
 		with open(self.CSV_FILE, 'r') as csvFile: # Open the CSV file under csvFile.
 			lines = csv.reader(csvFile, delimiter=',') # Grab the data.
@@ -111,30 +111,30 @@ class CsvManagement:
 							self.endArray.append(splitTimes[1]) # Append to endArray.
 		if self.ifMain: print("\n")
 		return self.recordCount # Return recordCount.
-		
-	
+
+
 	# >>> Process the CSV data.
 	def processCSVData(self):
-		
+
 		# >>> Replace the Month Names with the Month Numbers.
 		if self.ifMain: print("monthArray", self.monthArray, end="\n")
 		self.monthArray = [ str(self.MONTHS[monthName]).zfill(2) for monthName in self.monthArray ] # Switch Month names to Month numbers.
 		if self.ifMain: print("monthArray", self.monthArray, end="\n\n\n")
-		
+
 		# >>> Turn dayArray from string to integer values.
 		self.dayArray = [ str(day).zfill(2) for day in self.dayArray] # Make all values strings with leading Zeros.
-		
+
 		# >>> Make the start times 24Hr.
 		if self.ifMain: print("startArray", self.startArray, end="\n")
 		self.startArray = [ start.replace("AM", '').strip() for start in self.startArray ] # Strip AM.
 		if self.ifMain: print("startArray", self.startArray, end="\n\n\n")
-		
+
 		# >>> Make the end times 24HR.
 		if self.ifMain: print("endArray", self.endArray)
 		self.endArray = [ end.replace("PM", '').strip() for end in self.endArray ] # Strip PM.
 		self.endArray = [ str(int(end.split(':')[0]) + 12) + ':' + end.split(':')[1] for end in self.endArray ] # Format to 24Hr.
 		if self.ifMain: print("endArray", self.endArray, end="\n\n\n")
-		
+
 		# >>> Create and fill duration array.
 		for i in range(self.recordCount):
 			startTime = pd.to_datetime(self.startArray[i], format=self.TIME_FORMAT_STR) # Format and set the start time.
@@ -144,7 +144,7 @@ class CsvManagement:
 			self.paidHoursArray.append(diffTime - 0.50) # Subtract half an hour break, append result to array.
 		if self.ifMain: print("durationArray", self.durationArray, end="\n")
 		if self.ifMain: print("paidHoursArray", self.paidHoursArray, end="\n\n\n")
-		
+
 		# >>> Isolate the unnecessary months.
 		mostCommonMonth = mode(self.monthArray)
 		removedIndexes = []
@@ -152,53 +152,53 @@ class CsvManagement:
 		for i in range(len(self.monthArray)): # For every month ...
 			if self.monthArray[i] != mostCommonMonth: # ... if the month is not the most common month ...
 				removedIndexes.append(i) # ... add the index to the removedIndexes array.
-		
+
 		# >>> Remove the unnecessary months.
 		removedIndexes.reverse() # Reverse the list.
 		if self.ifMain: print(removedIndexes, end='\n\n\n')
 		for i in removedIndexes: # For every element in removedIndexes ...
 			del self.monthArray[i], self.dayArray[i], self.startArray[i], self.endArray[i], self.durationArray[i], self.paidHoursArray[i] # ... delete the index from all tables.
 		self.recordCount = self.recordCount - len(removedIndexes) # Update the record count.
-		
+
 		# >>> Print information IF this is __main__.
 		for i in range(len(self.dayArray)):
 			if self.ifMain: print("%s, %s, %s, %s, %s, %s" % (self.monthArray[i], self.dayArray[i], self.startArray[i], self.endArray[i], self.durationArray[i], self.paidHoursArray[i]))
 		if self.ifMain: print("\nrecordCount =", self.recordCount, end='\n\n') # Print the record count.
-		
-	
+
+
 	# >>> Combine data.
 	def combineData(self):
-		
+
 		# >>> Create the 2D table for all the data.
 		self.dataArray = self.create2DArray(len(self.FIELDS), self.recordCount)
-		
+
 		# >>> Create a row of combined data and add it to the dataArray.
 		for i in range(self.recordCount): # For every record ...
 			row = [self.monthArray[i], self.dayArray[i], self.startArray[i], self.endArray[i], self.durationArray[i], self.paidHoursArray[i]] # ... create a row of combined data ...
 			self.dataArray[i] = row # ... and add it to the dataArray.
-		
+
 		if self.ifMain: pp(self.dataArray)
-		
-		
+
+
 	# >>> Export all the data.
 	def exportAllData(self, allTablesData, tableNames):
-		
+
 		with open(self.CSV_EXPORT, 'w') as csvFile: # Prepare to write the csv file.
 			writer = csv.writer(csvFile)
-			
+
 			for table in allTablesData: # For every table,
 				writer.writerows(table) # write the table to the file.
-			
+
 		csvFile.close() # Close the csv file.
-		
-		
-		
+
+
+
 # ! - \/ \/ \/ \/ \/ - ! #
 # ! - - -  Main  - - - ! #
 if __name__ == "__main__":
-	
+
 	initCSV = CsvManagement() # Initiate the Class.
-	
+
 	initCSV.selectFile() # Select the file.
 	initCSV.getCSVData() # Get the CSV data.
 	initCSV.processCSVData() # Process this data.
--- _MainProgram.py
+++ _MainProgram.py
@@ -11,51 +11,51 @@ from DatabaseManagement import DatabaseManagement
 
 
 class Main:
-	
+
 	# ! - Functions - ! #
 	def __init__(self):
-		
+
 		# ! -  Classes  - ! #
 		self.mO = MenuOptions()
 		self.cM = CsvManagement()
 		self.dM = DatabaseManagement()
-		
+
 		self.dM.createPayDataTable() # Create the pay data table if it does not already exist.
-		
+
 		try: # Try to hash the database.
 			self.ORIGINAL_MD5 = self.dM.hashFile() # Save the original MD5 for the database.
 		except: # Else the database doesn't exist, so say so and move on.
 			self.ORIGINAL_MD5 = "0"
 			print("No db to hash yet.")
-		
+
 		self.CURRENT_YEAR  = str(dt.date.today().year)  # Set the current year.
 		self.CURRENT_MONTH = str(dt.date.today().month) # Set the current month.
 		self.CURRENT_DAY   = str(dt.date.today().day)   # Set the current day.
-		
+
 		self.continueProgram = True
-		
+
 		self.DIVIDER_DAY = "16"
 		self.PAY_DAY = "28"
-		
-		
+
+
 	# >>> Show the dashboard.
 	def showDashboard(self):
 		self.mO.workOutPaymentDay(self.CURRENT_YEAR, self.CURRENT_MONTH, self.PAY_DAY) # Update mO class paydates.
-		
+
 		if int(self.CURRENT_DAY) > int(self.mO.payDay): # If the current date is after this months payday, get next month.
 			self.mO.workOutPaymentDay(self.CURRENT_YEAR, str(int(self.CURRENT_MONTH) + 1), self.PAY_DAY) # Add one month if it's past the payday.
-		
+
 		tableName = "%s_%02d" % (self.mO.payYear, int(self.mO.payMonth)) # Get the month table name.
 		previousMonthTableName = "%s_%02d" % (self.mO.payYear, int(self.mO.payMonth) - 1) # Get the previous month table name.
-		
+
 		try:
 			payChequeValue = self.calculatePayChequeChoice(previousMonthTableName, tableName, False) # Try get the pay cheque value.
 		except:
 			payChequeValue = 0.00 # If it fails, set to ZERO.
-			
+
 		self.mO.dashboard(payChequeValue) # Launch the dashboard.
-		
-		
+
+
 	# >>> Calculate pay cheque.
 	def calculatePayChequeChoice(self, previousMonthTableName, tableName, doPrint):
 		previousMonthData, currentMonthData, shiftCount = self.dM.getPayChequeData(previousMonthTableName, tableName, self.DIVIDER_DAY) # Get the hours data.
@@ -66,8 +66,8 @@ class Main:
 		totalPay = previousMonthTotalHours * previousHourlyPay + currentMonthTotalHours * currentHourlyPay
 		payChequeValue = self.mO.printPayCheque(tableName, shiftCount, totalHours, totalPay, doPrint) # Print this data.
 		return payChequeValue
-	
-	
+
+
 	# >>> View month data.
 	def viewMonthDataChoice(self):
 		tableNames = self.dM.getTableNames() # Get the names of all the tables.
@@ -75,8 +75,8 @@ class Main:
 		if tableName == "quit": return # If the user wants to quit, we quit.
 		monthData = self.dM.getTableData(tableName) # Get the data from the table.
 		self.mO.printMonthData(tableName, monthData) # Print this data.
-		
-		
+
+
 	# >>> Import a new csv file.
 	def importANewCSVFileChoice(self):
 		self.cM.clearArrays() # Clear all of the arrays.
@@ -88,8 +88,8 @@ class Main:
 		tableName = self.dM.getTableNames()[0]
 		self.dM.saveNewPayData(tableName)
 		print("\nDone.", end='') # Tell the user that we are done importing the new csv file.
-		
-		
+
+
 	# >>> Edit pay data.
 	def editPayDataChoice(self):
 		tableNames = self.dM.getTableNames() # Get the names of all the tables.
@@ -98,55 +98,52 @@ class Main:
 		newPay = self.mO.inputNewPayPerHour() # Get the new pay per hour.
 		if newPay == "quit": return # If the user wants to quit, we quit.
 		self.dM.editPayDataTable(tableName, newPay) # Set the new pay amount for the tableName.
-		
-		
+
+
 	# >>> The beginning of the program.
 	def beginProgram(self):
-		
+
 		menuChoices = ("Calculate pay cheque","View month data","Import a new csv file","Edit pay data")
 		choice = self.mO.mainMenu(menuChoices) # Get a choice from the main menu.
-		
+
 		# >>> What now after choice?
 		match choice:
 			case '1': # "1) Calculate pay cheque."
 				try:
 					tableNames = self.dM.getTableNames() # Get the names of all the tables.
 					tableName = self.mO.choosePayCheque(tableNames, self.CURRENT_YEAR, self.CURRENT_MONTH, self.CURRENT_DAY) # Choose the month to calculate.
-					
 					if tableName == "quit": return # If the user wants to quit, we quit.
-					
 					previousMonthTableIndex = tableNames.index(tableName) + 1 # Get the index of the previous month.
 					previousMonthTableName = tableNames[previousMonthTableIndex] # Get the previous month table name.
-					
 					self.calculatePayChequeChoice(previousMonthTableName, tableName, True) # Run choice function.
 				except Exception as e:
 					self.mO.printErrorDetails(e, menuChoices[int(choice)-1]) # Print the custom error message.
-					
+
 			case '2': # "2) View month data."
 				try:
 					self.viewMonthDataChoice() # Run choice function.
 				except Exception as e:
 					self.mO.printErrorDetails(e, menuChoices[int(choice)-1]) # Print the custom error message.
-					
+
 			case '3': # "3) Import a new CSV file."
 				try:
 					self.importANewCSVFileChoice() # Run choice function.
 				except Exception as e:
 					self.mO.printErrorDetails(e, menuChoices[int(choice)-1]) # Print the custom error message.
-					
+
 			case '4': # "4) Edit pay data."
 				try:
 					self.editPayDataChoice() # Run choice function.
 				except Exception as e:
 					self.mO.printErrorDetails(e, menuChoices[int(choice)-1]) # Print the custom error message.
-					
+
 			case 'Q': # "Q) Quit."
 				self.mO.quitProgram() # Then quit the program.
-				
+
 				tableNames = self.dM.getTableNames() # Get the names of all the tables.
 				allTablesData = self.dM.getAllData(tableNames) # Get all of the data.
 				self.cM.exportAllData(allTablesData, tableNames) # Export all csv data.
-				
+
 				# >>> Check whether to run the shortcuts file.
 				try:
 					NEW_MD5 = self.dM.hashFile() # Save the new MD5 for the database.
@@ -155,36 +152,36 @@ class Main:
 						print("Adding new shifts to calendar.")
 				except Exception as e:
 					print("Failed to add shifts to calendar,\nplease do this manually.")
-					
+
 				self.continueProgram = False
-				
+
 			case _: # In case of invalid input.
 				print("\n\n! >>> ERROR - Invalid Input.\n\n") # Print an error warning.
 				self.mO.quitProgram() # Then quit the program.
 				self.continueProgram = False
-				
-				
+
+
 # ! - \/ \/ \/ \/ \/ - ! #
 # ! - - -  Main  - - - ! #
 if __name__ == "__main__":
-	
+
 	initMain = Main()
-	
+
 	initMain.showDashboard() # Show the dashboard at the very start.
-	
-	
+
+
 	while initMain.continueProgram: # Loop until we want to exit.
 		input("\nPress Enter to continue...") # Wait for user to be ready to continue.
 		initMain.beginProgram() # Run the menu on repeat.
-	
+
 	initMain.dM.connection.close() # Close the database connection.
-	
-	
-	
+
+
+
 	"""
-STUFF TO DO:
+	STUFF TO DO:
 
-- Make sure that this works at the new years.
-- Possibly add some shortcuts stuff.
+	- Make sure that this works at the new years.
+	- Possibly add some shortcuts stuff.
 
-"""
+	"""
